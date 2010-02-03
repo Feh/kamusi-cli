30049869ac83baafb865e939475c09fc055af294
Mirko Westermeier <mirko@westermeier.de>
2010-02-03 15:52:32 +0100

    biiiiig redesign of the whole import thing. seems to work correctly now.

--- convert.pl
+++ convert.pl
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use DBI;
 
-my %fields = (
+my %field = (
     class               => 'Class',
     derived_language    => 'Derived Language',
     derived_word        => 'Derived Word',
@@ -25,177 +25,81 @@ my %fields = (
     terminology         => 'Terminology',
 );
 
-my $dbargs = {AutoCommit => 0, PrintError => 1};
-my $dbh = DBI->connect("dbi:SQLite:dbname=words.db", "", "", $dbargs);
+my %field_dbname = reverse %field;
 
-# The creation command:
-#
-#
+my $valid_field_rx = join '|' => values %field;
 
-# recreate the tables from scratch
-# might throw an error message if the db has not existed in the first place.
-foreach my $lang (qw( english swahili )) {
+my %lang_file = (
+    english => 'ealla.txt', # TODO eall.txt, swahili => sall.txt
+);
+
+my %dbargs = (
+    AutoCommit  => 0,
+    PrintError  => 1,
+);
+
+# connect to the database
+my $dbh = DBI->connect(
+    'dbi:SQLite:dbname=words.db',
+    '', '', \%dbargs
+);
+
+# preparing the database for the data import
+foreach my $lang ( keys %lang_file ) {
     $dbh->do("DROP TABLE $lang");
     $dbh->do(
         "CREATE TABLE $lang ("
-            . join( ', ' => map "$_ TEXT" => keys %fields )
-            . ');'
+        . join( ', ' => map "$_ TEXT" => keys %field )
+        . ');'
     );
 }
 
-open(EN, "eall.txt");
-my %entry;
-while (<EN>) {
-    next unless /^\[/;
-    chomp;
-    my $value = $_;
-    $value =~ s/^\[[^\]]+\] //;
-    s/^\[([^\]]+)\].*$/$1/;
+my @fields = keys %field;   # a defined ordering of the %field keys
+                            # hashs do not have that, so we use this. Later.
 
-    if(/English Word/) { # commit array, then reset
-        $entry{"english_word"} = $value;
-
-        my $query = "insert into english (
-        class, derived_language, derived_word, dialect, english_definition,
-        english_example, english_plural, english_word, note,
-        part_of_speech, related_words, swahili_definition, swahili_example,
-        swahili_plural, swahili_word, taxonomy, terminology
-        ) values ( " .
-            $dbh->quote($entry{"class"}) . ", " .
-            $dbh->quote($entry{"derived_language"}) . ", " .
-            $dbh->quote($entry{"derived_word"}) . ", " .
-            $dbh->quote($entry{"dialect"}) . ", " .
-            $dbh->quote($entry{"english_definition"}) . ", " .
-            $dbh->quote($entry{"english_example"}) . ", " .
-            $dbh->quote($entry{"english_plural"}) . ", " .
-            $dbh->quote($entry{"english_word"}) . ", " .
-            $dbh->quote($entry{"note"}) . ", " .
-            $dbh->quote($entry{"part_of_speech"}) . ", " .
-            $dbh->quote($entry{"related_words"}) . ", " .
-            $dbh->quote($entry{"swahili_definition"}) . ", " .
-            $dbh->quote($entry{"swahili_example"}) . ", " .
-            $dbh->quote($entry{"swahili_plural"}) . ", " .
-            $dbh->quote($entry{"swahili_word"}) . ", " .
-            $dbh->quote($entry{"taxonomy"}) . ", " .
-            $dbh->quote($entry{"terminology"}) .
-        ");";
-
-        $dbh->do($query);
-        if ($dbh->err()) { die "$DBI::errstr\n"; }
-        %entry = ();
-    } # not the start of a word
-    elsif (/Class/) {
-        $entry{"class"} = $value;
-    } elsif (/Derived Language/) {
-        $entry{"derived_language"} = $value;
-    } elsif (/Derived Word/) {
-        $entry{"derived_word"} = $value;
-    } elsif (/Dialect/) {
-        $entry{"dialect"} = $value;
-    } elsif (/English Definition/) {
-        $entry{"english_definition"} = $value;
-    } elsif (/English Example/) {
-        $entry{"english_example"} = $value;
-    } elsif (/English Plural/) {
-        $entry{"english_plural"} = $value;
-    } elsif (/Note/) {
-        $entry{"note"} = $value;
-    } elsif (/Part of Speech/) {
-        $entry{"part_of_speech"} = $value;
-    } elsif (/Related Words/) {
-        $entry{"related_words"} = $value;
-    } elsif (/Swahili Definition/) {
-        $entry{"swahili_definition"} = $value;
-    } elsif (/Swahili Example/) {
-        $entry{"swahili_example"} = $value;
-    } elsif (/Swahili Plural/) {
-        $entry{"swahili_plural"} = $value;
-    } elsif (/Swahili Word/) {
-        $entry{"swahili_word"} = $value;
-    } elsif (/Taxonomy/) {
-        $entry{"taxonomy"} = $value;
-    } elsif (/Terminology/) {
-        $entry{"terminology"} = $value;
-    }
+my %insert_stmt;    # prepare the insert statements for each language
+                    # (a placeholder for the table name is not possible)
+foreach my $lang ( keys %lang_file ) {
+    $insert_stmt{$lang} = $dbh->prepare(
+        "INSERT INTO $lang ("
+        . join( ', ' => @fields )
+        . ') VALUES ('
+        . join( ', ' => ('?') x @fields )
+        . ')'
+    );
 }
-close(EN);
 
-open(SW, "sall.txt");
-%entry = ();
-while (<SW>) {
-    next unless /^\[/;
-    chomp;
-    my $value = $_;
-    $value =~ s/^\[[^\]]+\] //;
-    s/^\[([^\]]+)\].*$/$1/;
+# import the language data
+foreach my $lang ( keys %lang_file ) {
+    open my $filehandle, '<', $lang_file{$lang}
+        or die "Couldn't open file $lang_file{$lang}: $!\n";
 
-    if(/Swahili Word/) { # commit array, then reset
-        $entry{"swahili_word"} = $value;
+    my %record = ();
+    my $reading; # true if reading a record
 
-        my $query = "insert into swahili (
-        class, derived_language, derived_word, dialect, english_definition,
-        english_example, english_plural, english_word, note,
-        part_of_speech, related_words, swahili_definition, swahili_example,
-        swahili_plural, swahili_word, taxonomy, terminology
-        ) values ( " .
-            $dbh->quote($entry{"class"}) . ", " .
-            $dbh->quote($entry{"derived_language"}) . ", " .
-            $dbh->quote($entry{"derived_word"}) . ", " .
-            $dbh->quote($entry{"dialect"}) . ", " .
-            $dbh->quote($entry{"english_definition"}) . ", " .
-            $dbh->quote($entry{"english_example"}) . ", " .
-            $dbh->quote($entry{"english_plural"}) . ", " .
-            $dbh->quote($entry{"english_word"}) . ", " .
-            $dbh->quote($entry{"note"}) . ", " .
-            $dbh->quote($entry{"part_of_speech"}) . ", " .
-            $dbh->quote($entry{"related_words"}) . ", " .
-            $dbh->quote($entry{"swahili_definition"}) . ", " .
-            $dbh->quote($entry{"swahili_example"}) . ", " .
-            $dbh->quote($entry{"swahili_plural"}) . ", " .
-            $dbh->quote($entry{"swahili_word"}) . ", " .
-            $dbh->quote($entry{"taxonomy"}) . ", " .
-            $dbh->quote($entry{"terminology"}) .
-        ");";
+    while ( my $line = <$filehandle> ) {
 
-        $dbh->do($query);
-        if ($dbh->err()) { die "$DBI::errstr\n"; }
-        %entry = ();
-    } # not the start of a word
-    elsif (/Class/) {
-        $entry{"class"} = $value;
-    } elsif (/Derived Language/) {
-        $entry{"derived_language"} = $value;
-    } elsif (/Derived Word/) {
-        $entry{"derived_word"} = $value;
-    } elsif (/Dialect/) {
-        $entry{"dialect"} = $value;
-    } elsif (/English Definition/) {
-        $entry{"english_definition"} = $value;
-    } elsif (/English Example/) {
-        $entry{"english_example"} = $value;
-    } elsif (/English Plural/) {
-        $entry{"english_plural"} = $value;
-    } elsif (/English Word/) {
-        $entry{"english_word"} = $value;
-    } elsif (/Note/) {
-        $entry{"note"} = $value;
-    } elsif (/Part of Speech/) {
-        $entry{"part_of_speech"} = $value;
-    } elsif (/Related Words/) {
-        $entry{"related_words"} = $value;
-    } elsif (/Swahili Definition/) {
-        $entry{"swahili_definition"} = $value;
-    } elsif (/Swahili Example/) {
-        $entry{"swahili_example"} = $value;
-    } elsif (/Swahili Plural/) {
-        $entry{"swahili_plural"} = $value;
-    } elsif (/Taxonomy/) {
-        $entry{"taxonomy"} = $value;
-    } elsif (/Terminology/) {
-        $entry{"terminology"} = $value;
+        if ( $line =~ /^\[($valid_field_rx)\]\s+(.+)/ ) { # reading
+            $record{$1} = $2;
+            $reading++;
+        }
+        elsif ( $reading ) { # record finished
+            use Data::Dumper;
+            $insert_stmt{$lang}->execute( map {
+                exists $record{$_} ? $record{$_} : ''
+            } @field{@fields} );
+            %record     = ();
+            $reading    = undef;
+        }
+        else { # nothing to do
+            next; # no-op, but more readable
+        }
     }
+
+    close $filehandle;
 }
-close(EN);
 
 $dbh->commit();
 $dbh->disconnect();
+
+__END__
