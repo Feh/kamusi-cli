174a5d3e00ab220c669aeebcfe199089ffa4079f
Julius Plenz <kamusi@plenz.com>
2010-02-07 12:56:20 +0100

    simple positive/false positive statistics implemented
    So far these are pretty crappy results. The low fp rate for swahili
    words is because of the obviously too aggressive rule +5 for ending in
    a vowel.
    
    $VAR1 = 'en';
    $VAR2 = {
              'fp' => 16925,
              'found' => 14037
            };
    $VAR3 = 'sw';
    $VAR4 = {
              'fp' => 778,
              'found' => 21633
            };
    ./guess_language.pl  11.43s user 0.00s system 97% cpu 11.712 total

--- heuristics/guess_language.pl
+++ heuristics/guess_language.pl
@@ -1,12 +1,16 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use List::Util qw[max];
 use Data::Dumper;
 
 my %score = ( en => 0, sw => 0 );
-my $w = shift;
+my %lang_files = ( en => 'english_words.txt', sw => 'swahili_words.txt' );
 
-die 'no word!' unless $w;
+my %stat = (
+    en => { found => 0, fp => 0 },
+    sw => { found => 0, fp => 0 },
+);
 
 my %crit = (
     en => {
@@ -19,7 +23,7 @@ my %crit = (
             score => 20
         },
         unique => {
-            regex => '(ck|q|x)',
+            regex => '(ck|q|x|wh)',
             score => 30
         }
     },
@@ -31,6 +35,10 @@ my %crit = (
         },
         dgh => {
             regex => 'dh|gh',
+            score => 10
+        },
+        ku => {
+            regex => 'ku',
             score => 10
         },
         ana => {
@@ -52,11 +60,30 @@ my %crit = (
     }
 );
 
-foreach my $lang (keys %crit) {
-    foreach my $c (keys %{$crit{$lang}}) {
-        $score{$lang} += $crit{$lang}{$c}{score}
-            if $w =~ /$crit{$lang}{$c}{regex}/;
+foreach my $l (keys %crit) {
+    open my $fh, '<', $lang_files{$l} or die 'fail';
+    while(<$fh>) {
+        chomp;
+        foreach my $lang (keys %crit) {
+            $score{$lang} = 0;
+            foreach my $c (keys %{$crit{$lang}}) {
+                $score{$lang} += $crit{$lang}{$c}{score}
+                    if /$crit{$lang}{$c}{regex}/;
+            }
+            #print max(values %score) . "\n\n";
+        }
+        if($score{$l} == max(values %score)) {
+            if($score{$l} == 0) {
+                $stat{$l}{fp}++;
+            } else {
+                $stat{$l}{found}++;
+            }
+        } else {
+            $stat{$l}{fp}++;
+            # print $_ ."\n";
+            # print Dumper %score;
+        }
     }
 }
 
-print Dumper %score;
+print Dumper %stat;
