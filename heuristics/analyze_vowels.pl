dc6a2ba7119672e7da59944711f2c3aa58b33d49
Julius Plenz <kamusi@plenz.com>
2010-02-07 02:58:46 +0100

    scripts to analyze swahili consonants/vowels

---
+++ heuristics/analyze_vowels.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %c = ();
+
+open my $f, '<', 'swahili_words.txt' or die 'bleah';
+while(<$f>) {
+    chomp;
+    $c{$1}++ if /([aeiou]+)/;
+}
+
+map { print "$_ -> $c{$_}\n" } keys %c;
